1858666c3fb3ede0ab9def481fc5a41befda2f5e
Matteo Canu <matteo.canu@devise.it>
2013-08-06 12:59:27 +0200

    Added an extra new line after cat to avoid skipping the last line

--- inquisition.sh
+++ inquisition.sh
@@ -4,12 +4,12 @@ ereticlist="${1}"
 originalfile="${2}"
 
 if [ $# -ne 2 ]; then
-	echo ""
-	echo "	Usage: $0 <list> <target file>"
-	echo "	<list> is a file containg the list of unused classes that need to be removed, 1 per line, no trailing {"
-	echo "	<target file> is a non minified CSS file (1 line 1 rule)"
-	echo ""
-	exit
+  echo ""
+  echo "  Usage: $0 <list> <target file>"
+  echo "  <list> is a file containg the list of unused classes that need to be removed, 1 per line, no trailing {"
+  echo "  <target file> is a non minified CSS file (1 line 1 rule)"
+  echo ""
+  exit
 fi
 
 function speak() { 
@@ -18,14 +18,14 @@ function speak() {
 
 function checkforeresy() {
  if [ -e "$@" ]; then
-	if [ ! -r "$@" ]; then
-	 	#If it exist but is not readable we notify and skip the database optmization
-		speak "[NOTICE] $@ not readable, quitting."
-		exit
-	fi
+  if [ ! -r "$@" ]; then
+    #If it exist but is not readable we notify and skip the database optmization
+    speak "[NOTICE] $@ not readable, quitting."
+    exit
+  fi
  else
-	speak "[ERROR] $@ doesn't exist, quitting."
-	exit
+  speak "[ERROR] $@ doesn't exist, quitting."
+  exit
  fi
 }
 
@@ -35,8 +35,8 @@ checkforeresy "$ereticlist"
 checkforeresy "$originalfile"
 
 if [ -e "clean-${originalfile}" ]; then
-	speak "[ERROR] clean-${originalfile} already exist, remove it before executing this script again, quitting."
-	exit
+  speak "[ERROR] clean-${originalfile} already exist, remove it before executing this script again, quitting."
+  exit
 fi
 
 initiallines=$( cat "${originalfile}" | wc -l )
@@ -47,11 +47,15 @@ cleanfile="clean-${originalfile}"
 
 speak "Created ${cleanfile}"
 
-cat "${ereticlist}" | while read eretic; do
-	grep -v "^${eretic} {" "${cleanfile}" > "1-${cleanfile}"
-	mv "1-${cleanfile}" "${cleanfile}"
-	grep -v "^${eretic}{" "${cleanfile}" > "1-${cleanfile}"
-	mv "1-${cleanfile}" "${cleanfile}"
+(cat "${ereticlist}";echo) | while read eretic; do
+  if [ -z "${eretic}" ]; then
+    continue
+  fi
+  speak "${eretic}"
+  grep -v "^${eretic} {" "${cleanfile}" > "1-${cleanfile}"
+  mv "1-${cleanfile}" "${cleanfile}"
+  grep -v "^${eretic}{" "${cleanfile}" > "1-${cleanfile}"
+  mv "1-${cleanfile}" "${cleanfile}"
 done
 
 newlines=$( cat "${cleanfile}" | wc -l )
